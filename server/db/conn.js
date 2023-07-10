995e5e997412ca2d80e435051b052d5328c9acac
Pujita Chakraborty <117858130+pujita789@users.noreply.github.com>
2023-07-10 21:05:03 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const  { connect } = require ('mongoose');// added commonJS syntax for error resolving.
 
 const DB = process.env.DATABASE
 
-mongoose.connect(DB,{
+connect(DB,{
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(()=> console.log("DataBase Connected")).catch((errr)=>{
